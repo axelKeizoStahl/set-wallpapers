5f02dcb6c33e9df6edf5ac4e5920e920c453f06f
axelKeizoStahl <axelkeizo@gmail.com>
2023-08-22 19:21:35 -0400

    using environment variable instead of apikey

--- set-wallpaper.sh
+++ set-wallpaper.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 FILE=${FILE:-~/tmp/random-wallpaper}
 current_resolution=$(xdpyinfo | grep -oP 'dimensions:\s+\K\S+')
-echo $current_resolution
-api_url="https://wallhaven.cc/api/v1/search?api_key=GoWbV0JR1NH2rBD38wxsbYmAvUswhosy&purity=100&sorting=random&order=desc&resolutions=$current_resolution"
+# echo $current_resolution
+if !$WALLHAVEN_API_KEY; then
+    echo "API_KEY is not set, go to https://wallhaven.cc/join to get a wallhaven API key"
+    exit 1
+fi
+api_url="https://wallhaven.cc/api/v1/search?api_key=$WALLHAVEN_API_KEY&purity=100&sorting=random&order=desc&resolutions=$current_resolution"
 # echo $api_url
 download_url=$(curl -L "$api_url" | jq -r '.data[0].path')
 # echo $download_url
