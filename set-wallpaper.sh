a7131985e12ee011b730bf2f6a9ec1c4274ecd5a
axelKeizoStahl <axelkeizo@gmail.com>
2023-08-22 20:47:59 -0400

    added only anime flag and allowed sketchy

--- set-wallpaper.sh
+++ set-wallpaper.sh
@@ -6,7 +6,7 @@ if !$WALLHAVEN_API_KEY; then
     echo "API_KEY is not set, go to https://wallhaven.cc/join to get a wallhaven API key"
     exit 1
 fi
-api_url="https://wallhaven.cc/api/v1/search?api_key=$WALLHAVEN_API_KEY&purity=100&sorting=random&order=desc&resolutions=$current_resolution"
+api_url="https://wallhaven.cc/api/v1/search?api_key=$WALLHAVEN_API_KEY&categories=010&purity=110&sorting=random&order=desc&resolutions=$current_resolution"
 # echo $api_url
 download_url=$(curl -L "$api_url" | jq -r '.data[0].path')
 # echo $download_url
